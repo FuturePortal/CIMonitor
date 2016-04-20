a793d46559b84764b8621aab2d5b5d85d19c7883
Rick van der Staaij <info@rickvanderstaaij.nl>
2016-04-20 21:35:27 +0200

    Switch the traffic light's lamps on and off

--- app/StatusModule/TrafficLight.js
+++ app/StatusModule/TrafficLight.js
@@ -2,6 +2,9 @@ const util = require('util');
 var StatusModule = require('./StatusModule');
 var exec = require('child_process').exec;
 
+const ON = 0;
+const OFF = 1;
+
 /**
  * TrafficLight
  *
@@ -31,29 +34,42 @@ TrafficLight.prototype.init = function() {
  * @param {object} status
  */
 TrafficLight.prototype.handleStatus = function(status) {
-    switch (status.status) {
-        case 'success':
-            break;
-        case 'started':
-            break;
-        case 'failure':
-            break;
+    var greenLight = ON;
+    var orangeLight = OFF;
+    var redLight = OFF;
+
+    if (this.statusManager.hasStartedStatus()) {
+        greenLight = OFF;
+        orangeLight = ON;
     }
+
+    if (this.statusManager.hasFailureStatus()) {
+        greenLight = OFF;
+        redLight = ON;
+    }
+
+    exec('gpio write ' + this.greenPin + ' ' + greenLight);
+    exec('gpio write ' + this.orangePin + ' ' + orangeLight);
+    exec('gpio write ' + this.redPin + ' ' + redLight);
+    console.log(
+        '[TrafficLight] Green is ' + (greenLight === ON) ? 'on' : 'off' + ', orange is'
+        + (orangeLight === ON) ? 'on' : 'off' + ', and red is ' + (redLight === ON) ? 'on' : 'off' + '.'
+    );
 };
 
 /**
- * Prepares the gpio pins, turns on the green light
+ * Prepares the gpio pins. Turns the green light on, turns the orange and red light off.
  */
 TrafficLight.prototype.prepareRelay = function() {
     exec('gpio mode ' + this.redPin + ' out');
-    exec('gpio write ' + this.redPin + ' 1');
+    exec('gpio write ' + this.redPin + ' ' + OFF);
     exec('gpio mode ' + this.orangePin + ' out');
-    exec('gpio write ' + this.orangePin + ' 1');
+    exec('gpio write ' + this.orangePin + ' ' + OFF);
     exec('gpio mode ' + this.greenPin + ' out');
-    exec('gpio write ' + this.greenPin + ' 0');
+    exec('gpio write ' + this.greenPin + ' ' + ON);
     console.log(
         '[TrafficLight] Set gpio pin ' + this.redPin + ', ' + this.orangePin + ' and ' + this.greenPin
-        + ' to output mode and switched off.'
+        + ' to output mode and switched off. Green light is on.'
     );
 };
 
