cab6ea571fd6fd37224f64309c84c46d99596621
Rick van der Staaij <rick@enrise.com>
2016-04-29 22:19:54 +0200

    Add travis build with eslint

--- gulpfile.js
+++ gulpfile.js
@@ -36,20 +36,16 @@ gulp.task('server:restart', function() {
 });
 
 gulp.task('eslint', function () {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
     return gulp.src(jsFiles)
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
         .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format());
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        //.pipe(eslint.failAfterError());
+});
+
+gulp.task('eslint-fail-on-error', function () {
+    return gulp.src(jsFiles)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['server:start', 'eslint'], function() {
