ed8ebbe327eb0ca5aa8c70112a2cf51b644ec110
Rick van der Staaij <info@rickvanderstaaij.nl>
2017-05-25 21:45:04 +0200

    Make the status-block content dynamic

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,86 @@
 Vue.component('status-block', {
     template:
-    '<div class="status">' +
+    '<div class="status" :class="status.status">' +
     '   <div class="status-icon-box">' +
-    '       <img src="/images/types/deploy.svg" />' +
+    '       <img :src="getTypeImage" />' +
     '   </div>' +
     '   <div class="status-title">' +
-    '       <span class="title">CIMonitor</span>' +
-    '       <span class="sub-title">master</span>' +
+    '       <span class="title">{{ status.name }}</span>' +
+    '       <span class="sub-title">{{ status.branch }}</span>' +
     '   </div>' +
-    '   <div class="status-stages job-container">' +
-    '       <div class="success"><span>test</span></div>' +
-    '       <div class="success"><span>build</span></div>' +
-    '       <div><span>deploy</span></div>' +
-    '       <div class="todo"><span>acceptance</span></div>' +
+    '   <div class="status-stages job-container" v-if="status.stages">' +
+    '       <div v-for="stage in status.stages" :class="stage.status">' +
+    '           <span>{{ stage.name }}</span>' +
+    '       </div>' +
     '   </div>' +
-    '   <div class="status-jobs job-container">' +
-    '       <div><span>deploying to production</span></div>' +
+    '   <div class="status-jobs job-container" v-if="status.jobs">' +
+    '       <div v-for="job in status.jobs" :class="job.status">' +
+    '           <span>{{ job.name }}</span>' +
+    '       </div>' +
     '   </div>' +
     '   <div class="status-info">' +
-    '       <div class="status-face" style="background-image: url(\'https://rickvanderstaaij.nl/images/rick.jpg\')">' +
-    '       </div>' +
+    '       <div' +
+    '           v-if="status.photo"' +
+    '           class="status-face"' +
+    '           :style="getStatusPhoto"' +
+    '       ></div>' +
     '       <div class="status-time">' +
-    '           5 minutes ago' +
+    '           {{ time }}' +
     '       </div>' +
     '   </div>' +
-    '</div>'
+    '</div>',
+    props: ['status'],
+    data: function() {
+        return {
+            'time': 'now',
+        };
+    },
+    methods: {
+
+    },
+    computed: {
+        getTypeImage: function() {
+            return '/images/types/' + this.status.type + '.svg';
+        },
+        getStatusPhoto: function() {
+            return 'background-image: url(\'' + this.status.photo + '\')'
+        }
+    }
 });
 
 Vue.component('status-overview', {
     template:
     '<div>' +
-        '<status-block></status-block>' +
-        '<status-block></status-block>' +
-    '</div>'
+    '   <status-block' +
+    '       v-for="status in getStatuses()"' +
+    '       :key="status.time"' +
+    '       :status="status"' +
+    '   ></status-block>' +
+    '</div>',
+    data: function() {
+        return {
+            statuses: [],
+        };
+    },
+    methods: {
+        getStatuses: function() {
+            if (this.statuses.length === 0) {
+                return [{
+                    name: 'No statuses yet',
+                    type: 'wait',
+                    branch: '',
+                    status: 'success',
+                    time: (new Date()).getMilliseconds(),
+                    jobs: [{
+                        name: 'Waiting for new statuses',
+                        status: 'success',
+                    }],
+                }];
+            }
+
+            return this.statuses;
+        },
+    }
 });
 
 new Vue({
