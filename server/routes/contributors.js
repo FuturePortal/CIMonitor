84f887f1cf80f9b6dbf70733b258cdd81da19de2
Rick van der Staaij <info@rickvanderstaaij.nl>
2018-11-26 13:24:43 +0100

    Fetch user details for the contributors

--- server/routes/contributors.js
+++ server/routes/contributors.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = (module.exports = express());
-var httpRequest = require('request');
+var requestPromise = require('request-promise');
 
 const getContributorsInfo = contributors =>
     contributors
@@ -12,10 +12,64 @@ const getContributorsInfo = contributors =>
         }))
         .sort((contributorA, contributorB) => contributorB.commits - contributorA.commits);
 
+const getRequestObject = uri => ({
+    uri,
+    headers: {
+        'User-Agent': 'github.com/CIMonitor/CIMonitor',
+        Accept: 'application/json',
+    },
+});
+
 app.get('/', (request, response) => {
     console.log('/contributors [GET]');
 
-    httpRequest(
+    requestPromise(getRequestObject('https://api.github.com/repos/CIMonitor/CIMonitor/stats/contributors'))
+        .then(body => {
+            console.log(`[Contributors] Fetched simple list of the contributors from GitHub.`);
+
+            const contributors = getContributorsInfo(JSON.parse(body));
+            const fetchUserDetailPromises = [];
+            const detailedContributors = [];
+
+            contributors.forEach(simpleContributor => {
+                const fetchDetails = requestPromise(
+                    getRequestObject(`https://api.github.com/users/${simpleContributor.username}`)
+                )
+                    .then(body => {
+                        const userDetails = JSON.parse(body);
+                        console.log(`[Contributors] Fetched details for ${simpleContributor.username}.`);
+
+                        detailedContributors.push({
+                            ...simpleContributor,
+                            name: userDetails.name,
+                            blog: userDetails.blog,
+                            location: userDetails.location,
+                        });
+                    })
+                    .catch(() => {
+                        console.log(`[Contributors] Failed to fetch details for ${simpleContributor.username}.`);
+
+                        detailedContributors.push(simpleContributor);
+                    });
+
+                fetchUserDetailPromises.push(fetchDetails);
+            });
+
+            Promise.all(fetchUserDetailPromises).then(() => {
+                console.log(`[Contributors] Pushing list of the contributors.`);
+
+                response.json({
+                    message: 'Showing a list af all contributors to CIMonitor.',
+                    contributors: detailedContributors,
+                });
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            return response.json({ message: 'Failed to get a list of contributors from GitHub.' }, 500);
+        });
+
+    /*httpRequest(
         {
             uri: 'https://api.github.com/repos/CIMonitor/CIMonitor/stats/contributors',
             headers: {
@@ -28,16 +82,14 @@ app.get('/', (request, response) => {
                 return response.json({ message: 'Failed to get a list of contributors from GitHub.' }, 500);
             }
 
-            try {
-                const contributors = JSON.parse(httpBody);
-                response.json({
-                    message: 'Showing a list af all contributors to CIMonitor.',
-                    contributors: getContributorsInfo(contributors),
-                });
-            } catch (error) {
-                console.log(error);
-                return response.json({ message: 'Internal error formatting contributors' }, 500);
-            }
+            const contributors = getContributorsInfo(JSON.parse(httpBody));
+
+            // promise all users de moeder
+
+            response.json({
+                message: 'Showing a list af all contributors to CIMonitor.',
+                contributors: contributors,
+            });
         }
-    );
+    );*/
 });
