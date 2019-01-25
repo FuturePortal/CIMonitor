1aa6e92bbd2b60dc371d3e7e5a52c391b823e6d8
Steef Min <steefmin@gmail.com>
2019-01-25 21:00:18 +0100

    Add hostname in the page title to differentiate multiple dashboards (#110)
    * add hostname in dashboard title
    
    * move title in hostname to dashboard component
    
    * move the host location in title to dashboard creation
    
    * use ES6 and use fixed title prefix

--- front-end/dashboard.js
+++ front-end/dashboard.js
@@ -24,6 +24,9 @@ new Vue({
             isConnected: false,
         };
     },
+    created() {
+        document.title = `CIMonitor | ${location.host}`;
+    },
     methods: {
         updateFavicon(globalState) {
             document
