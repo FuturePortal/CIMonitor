ada8664beca91fa9f556ede775ea22e21fa5ee9b
Rick van der Staaij <6495166+RickvdStaaij@users.noreply.github.com>
2019-01-25 21:53:45 +0100

    Remove themes since we have an awesome one now that fits all (#112)
    * Removed status template files and scoped css in the components
    
    * Remove theme from the settings store
    
    * Make the error-darker color less extremely dark
    
    * Remove last theme reference

--- front-end/components/Status/JobsAndStages/jobs-and-stages.vue
+++ front-end/components/Status/JobsAndStages/jobs-and-stages.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="status__detail-jobs-and-stages">
-        <div class="status__detail-stages" v-if="stages.length > 0">
-            <div class="status__detail-stage" :class="getStageStatus(stage)" v-for="stage in stages" :key="stage">
+    <div class="detail-jobs-and-stages">
+        <div class="detail-stages" v-if="stages.length > 0">
+            <div class="detail-stage" :class="getStageStatus(stage)" v-for="stage in stages" :key="stage">
                 {{ stage }}
             </div>
         </div>
-        <div class="status__detail-jobs" v-if="interestingJobs.length > 0">
-            <div class="status__detail-job" v-for="job in interestingJobs" :class="job.state" :key="job.name">
+        <div class="detail-jobs" v-if="interestingJobs.length > 0">
+            <div class="detail-job" v-for="job in interestingJobs" :class="job.state" :key="job.name">
                 {{ job.name }}
             </div>
         </div>
@@ -61,7 +61,78 @@ export default {
 </script>
 
 <style lang="sass" rel="stylesheet/sass" scoped>
-.status__detail-jobs,
-.status__detail-stages
+.detail-jobs,
+.detail-stages
     font-size: 26px
+    display: flex
+    width: 100%
+
+.detail-stage
+    margin: 5px 0
+
+    &:first-child
+        margin-left: 5px
+        border-top-left-radius: 5px
+        border-bottom-left-radius: 5px
+
+    &:last-child
+        margin-right: 5px
+        border-top-right-radius: 5px
+        border-bottom-right-radius: 5px
+
+.detail-job
+    border-radius: 5px
+    margin: 5px
+
+.detail-stage,
+.detail-job
+    flex: 1
+    padding: 5px 10px
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
+    min-width: 0
+    flex-shrink: 1
+
+    &::before
+        font-family: "Font Awesome 5 Free"
+        font-weight: 900
+
+    &.error::before
+        content: "\f00d"
+
+    &.running::before
+        animation: fa-spin 2s infinite linear
+        display: inline-block
+        content: "\f2f9"
+
+    &.pending
+        opacity: 0.7
+
+        &::before
+            content: "\f3c5"
+
+    &.success::before
+        content: "\f00c"
+
+    &.warning::before
+        content: "\f071"
+
+    &.info::before
+        content: "\f05a"
+
+    &.error
+        background: $color-error-darker
+
+    &.running
+        background: $color-warning-darker
+
+    &.success
+        background: $color-success-darker
+
+    &.warning
+        background: $color-warning-darker
+
+    &.info
+        background: $color-info-darker
 </style>
