dac5b2a252a7f39c5a5caed1097422a851e887f7
Rick van der Staaij <info@rickvanderstaaij.nl>
2020-02-16 12:05:49 +0100

    Add customization to switch to the small toolbar

--- front-end/store/modules/SettingStore.js
+++ front-end/store/modules/SettingStore.js
@@ -16,12 +16,15 @@ import {
     SETTINGS_SET_PASSWORD,
     SETTINGS_CLEAR_PASSWORD,
     SETTINGS_SET_PASSWORD_REQUIRED,
+    SETTINGS_SET_TOOLBAR_SMALL,
+    SETTINGS_SET_TOOLBAR_LARGE,
 } from '../StaticMutations';
 import API from '../../classes/api.js';
 
 const state = {
     settingsPanelOpen: false,
     pushNotifications: false,
+    smallToolbar: false,
     notificationStatuses: {
         info: true,
         warning: true,
@@ -77,6 +80,14 @@ const mutations = {
 
     [SETTINGS_CLEAR_PASSWORD](state) {
         state.password = null;
+    },
+
+    [SETTINGS_SET_TOOLBAR_SMALL](state) {
+        state.smallToolbar = true;
+    },
+
+    [SETTINGS_SET_TOOLBAR_LARGE](state) {
+        state.smallToolbar = false;
     },
 
     [SETTINGS_SET_PASSWORD_REQUIRED](state, passwordRequired) {
