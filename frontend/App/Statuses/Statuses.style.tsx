f75cbacca080e6fee20fa5ecec651c935f0b4765
Rick van der Staaij <id@rick.nu>
2022-03-14 19:46:35 +0100

    Create dull list of the statuses on the dashboard

---
+++ frontend/App/Statuses/Statuses.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const List = styled.div``;
+
+export const Status = styled.div``;
+
+export const Process = styled.div``;
+
+export const Stage = styled.div``;
+
+export const Step = styled.div``;
--- frontend/App/Statuses/Statuses.tsx
+++ frontend/App/Statuses/Statuses.tsx
@@ -1,11 +1,45 @@
 import { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import { getStatuses } from '/frontend/store/status/selectors';
+import { List, Status, Process, Stage, Step } from './Statuses.style';
 
 const Statuses = (): ReactElement => {
-    const status = useSelector(getStatuses);
+    const statuses = useSelector(getStatuses);
 
-    return <pre>{JSON.stringify(status, null, 4)}</pre>;
+    return (
+        <List>
+            {statuses.length === 0 && <h1>Nothing yet.</h1>}
+            {statuses.map((status) => (
+                <Status>
+                    <h1>
+                        {status.project}: {status.state}
+                    </h1>
+                    <p>
+                        {status.branch} {status.tag}
+                    </p>
+                    {status.processes.map((process) => (
+                        <Process>
+                            <h2>
+                                {process.title}: {process.state}
+                            </h2>
+                            {process.stages.map((stage) => (
+                                <Stage>
+                                    <h3>
+                                        {stage.title}: {stage.state}
+                                    </h3>
+                                    {stage.steps.map((step) => (
+                                        <Step>
+                                            {step.title}: {step.state}
+                                        </Step>
+                                    ))}
+                                </Stage>
+                            ))}
+                        </Process>
+                    ))}
+                </Status>
+            ))}
+        </List>
+    );
 };
 
 export default Statuses;
