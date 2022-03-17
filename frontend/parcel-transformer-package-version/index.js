4248bca2805c3b309d8871c09a763e3b948babd7
Rick van der Staaij <id@rick.nu>
2022-03-17 23:52:24 +0100

    Frustrate about the not working build translator and add readthedocs webhook

--- frontend/parcel-transformer-package-version/index.js
+++ frontend/parcel-transformer-package-version/index.js
@@ -2,13 +2,26 @@ const plugin = require('@parcel/plugin');
 const path = require('path');
 
 module.exports = new plugin.Transformer({
-    async transform({ asset, config, options }) {
+    // async canReuseAST() {
+    //     return false;
+    // },
+    // async parse({ asset }) {
+    //     return {
+    //         type: 'my-compiler',
+    //         version: '1.0.0',
+    //         program: await asset.getCode(),
+    //     };
+    // },
+    async transform({ asset }) {
         let source = await asset.getCode();
 
-        const package = require(path.resolve('./package.json'));
+        const package = require(path.resolve(process.cwd(), 'package.json'));
 
         asset.setCode(source.replace('PACKAGE_VERSION', package.version));
 
         return [asset];
     },
+    // async generate({ ast }) {
+    //     return ast.program;
+    // },
 });
