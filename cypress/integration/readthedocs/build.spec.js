3c44058754bbbcdf84a414c438e0a28f3446bd5c
Rick van der Staaij <id@rick.nu>
2023-11-05 21:17:35 +0100

    Bump package version to 4.11.1

--- cypress/integration/readthedocs/build.spec.js
+++ cypress/integration/readthedocs/build.spec.js
@@ -1,20 +1,20 @@
 /// <reference types="cypress" />
 
 context('A running read the docs push', () => {
-    it('opens the CIMonitor dashboard', () => {
-        cy.visit('/');
-    });
+	it('opens the CIMonitor dashboard', () => {
+		cy.visit('/');
+	});
 
-    it('pushes a failed build', () => {
-        cy.readthedocs(`build-failed/1`);
-        cy.wait(1000);
-        cy.readthedocs(`build-failed/2`);
-    });
+	it('pushes a failed build', () => {
+		cy.readthedocs(`build-failed/1`);
+		cy.wait(1000);
+		cy.readthedocs(`build-failed/2`);
+	});
 
-    it('pushes a successful build', () => {
-        cy.wait(2000);
-        cy.readthedocs(`build-success/1`);
-        cy.wait(1000);
-        cy.readthedocs(`build-success/2`);
-    });
+	it('pushes a successful build', () => {
+		cy.wait(2000);
+		cy.readthedocs(`build-success/1`);
+		cy.wait(1000);
+		cy.readthedocs(`build-success/2`);
+	});
 });
