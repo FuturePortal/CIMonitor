19b7bca0e2656205d60d51d89b73cae5f163fe00
Rick van der Staaij <id@rick.nu>
2023-11-05 21:12:39 +0100

    Make pull-request test up to date with new test data set

--- cypress/integration/github/pull-request.spec.js
+++ cypress/integration/github/pull-request.spec.js
@@ -6,17 +6,10 @@ context('A running GitHub pull-request push', () => {
 	});
 
 	it('creates a pull request with action flow', () => {
-		for (let count = 1; count <= 9; count++) {
+		for (let count = 1; count <= 23; count++) {
 			if (count > 1) {
 				cy.wait(300);
 			}
-			cy.github(`pull-request/${count}`);
-		}
-	});
-
-	it('pushes a new commit', () => {
-		for (let count = 10; count <= 18; count++) {
-			cy.wait(300);
 			cy.github(`pull-request/${count}`);
 		}
 	});
