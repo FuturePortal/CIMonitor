27ae8427734ec9209bab44a4c21eb8194fa66ef2
Stefan van Essen <git@stefan-van-essen.nl>
2023-12-04 12:34:37 +0100

    Prioritize running job over success or failed status

--- frontend/store/status/selectors.ts
+++ frontend/store/status/selectors.ts
@@ -5,12 +5,12 @@ import Status, { State } from '/types/status';
 export const getStatuses = (state: RootState): Status[] => state.status.statuses;
 
 export const getGlobalState = (state: RootState): State => {
-	if (state.status.statuses.find((status) => status.state === 'error')) {
-		return 'error';
-	}
-
 	if (state.status.statuses.find((status) => status.state === 'warning')) {
 		return 'warning';
+	}
+
+	if (state.status.statuses.find((status) => status.state === 'error')) {
+		return 'error';
 	}
 
 	return 'success';
