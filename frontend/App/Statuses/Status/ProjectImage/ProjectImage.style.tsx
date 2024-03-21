ed5fec3149da180d0090dce7cbde0c96ddb792b2
Rick van der Staaij <6495166+rick-nu@users.noreply.github.com>
2024-03-21 12:57:06 +0100

    Improve status header and its images for mobile (#207)

--- frontend/App/Statuses/Status/ProjectImage/ProjectImage.style.tsx
+++ frontend/App/Statuses/Status/ProjectImage/ProjectImage.style.tsx
@@ -1,11 +1,16 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-	margin-right: 1rem;
+import { fromSize } from '/frontend/style/size';
 
-	img {
-		border-radius: 0.25rem;
-		max-width: 6rem;
-		max-height: 6rem;
-	}
+export const ProjectAvatar = styled.img`
+	border-radius: 0.25rem;
+	width: 3rem;
+	height: 3rem;
+	grid-row: 1;
+	grid-column: 1;
+
+	${fromSize.medium(css`
+		width: 6rem;
+		height: 6rem;
+	`)}
 `;
